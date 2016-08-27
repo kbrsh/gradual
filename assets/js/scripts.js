0912bfdc5de75d5c1913ba9e5fe2091652d9e84e
Gopal Shah <simplecooldude1@gmail.com>
2016-08-26 22:23:00 -0700

    close nav on link click

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+document.querySelector('.button_container').addEventListener("click", function() {
+   document.querySelector('.button_container').classList.toggle('active');
+   document.querySelector('.overlay').classList.toggle('open');
+ });
+
+ var menuLinks = document.querySelectorAll(".menuLink");
+
+ for(var i = 0; i < menuLinks.length; i++) {
+   menuLinks[i].addEventListener("click", function() {
+     document.querySelector('.button_container').classList.toggle('active');
+     document.querySelector('.overlay').classList.toggle('open');
+   });
+ }
