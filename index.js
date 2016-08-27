465fec63e167a76ea1591cb440a9dc048fa2bbcd
Gopal Shah <simplecooldude1@gmail.com>
2016-08-26 18:35:38 -0700

    gradual log

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/assets'));
 
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function (req, res) {
-    util.log("[SNIP] Listening", "green");
+    util.log("[GRADUAL] Listening", "green");
 });
 
 module.exports.app = app;
