95a63e02345fa6003d542e71995bde1173b2481a
Gopal Shah <simplecooldude1@gmail.com>
2016-08-26 20:14:17 -0700

    get middleware

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ var session = require("express-session");
 var renderIndex = require("./src/renderindex.js");
 var model = require("./models/model.js");
 var everyauth = require('everyauth');
+var cookieParser = require("cookieParser");
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/assets'));
+app.use(cookieParser('mr ripley'))
+app.use(session())
+app.use(everyauth.middleware(app));
 
 app.get("/", function(req, res, next) {
   res.set("Content-Type", "text/html");
