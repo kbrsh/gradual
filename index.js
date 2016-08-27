7c11a0c41500f8e4f0c5fec787b03d362ac55c79
Gopal Shah <simplecooldude1@gmail.com>
2016-08-26 18:50:38 -0700

    ditch

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require("express");
 var app = express();
 var util = require('./src/util.js');
+var bodyParser = require("body-parser");
+var renderindex = require("../src/renderindex.js");
+var model = require("../models/model.js");
 
-// app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/assets'));
+
+
+app.get("/", function(req, res) {
+  res.set("Content-Type", "text/html");
+  res.end(renderindex.render(""));
+});
 
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function (req, res) {
     util.log("[GRADUAL] Listening", "green");
