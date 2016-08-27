3ea44939623180be3730fcd89c1b9921119cbc73
Gopal Shah <simplecooldude1@gmail.com>
2016-08-26 18:25:05 -0700

    model

---
+++ models/model.js
@@ -0,0 +1,22 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('database', 'admin', 'pass', {
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  logging: false,
+
+  storage: 'database.sqlite'
+});
+
+
+var user = sequelize.define('USER', {
+    username: { type: Sequelize.STRING(), unique: true, primaryKey: true },
+    todos: Sequelize.STRING()
+});
+
+sequelize.sync();
