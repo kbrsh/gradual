9992e96ddb375cdf2fd28b45ae2e1f26469d72e3
Gopal Shah <simplecooldude1@gmail.com>
2016-08-30 18:38:01 -0700

    random gradient for reference

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,1 @@
-
-
-document.body
+// document.body.style.background = "linear-gradient(to bottom right, #" + Math.random().toString(16).slice(2, 8) + ", #" + Math.random().toString(16).slice(2, 8) + ")";
