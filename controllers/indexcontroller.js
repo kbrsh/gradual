856478fe986203ec8e8bc381fcdcfcc7d20007c8
Gopal Shah <simplecooldude1@gmail.com>
2016-08-30 18:52:09 -0700

    clean up index controller

--- controllers/indexcontroller.js
+++ controllers/indexcontroller.js
@@ -2,12 +2,8 @@ var fs = require('fs');
 
 var template = fs.readFileSync("./views/index/index.html", "utf-8");
 
-var render = function(username) {
-  if(username) {
-    return template.replace(/{{username}}/g, username);
-  } else {
-    return template;
-  }
+var render = function() {
+  return template;
 }
 
 module.exports.render = render;
