bb44d573767168acaef298508bfe2fd2dc3ce3d9
Gopal Shah <simplecooldude1@gmail.com>
2016-08-30 18:34:15 -0700

    remove menu code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,3 @@
-document.querySelector('.button_container').addEventListener("click", function() {
-   document.querySelector('.button_container').classList.toggle('active');
-   document.querySelector('.overlay').classList.toggle('open');
- });
 
- var menuLinks = document.querySelectorAll(".menuLink");
 
- for(var i = 0; i < menuLinks.length; i++) {
-   menuLinks[i].addEventListener("click", function() {
-     document.querySelector('.button_container').classList.toggle('active');
-     document.querySelector('.overlay').classList.toggle('open');
-   });
- }
+document.body
