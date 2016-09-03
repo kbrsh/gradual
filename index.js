5cd3c762314fc8cf07a616299d8da5d0a1f762de
Gopal Shah <simplecooldude1@gmail.com>
2016-09-02 19:15:59 -0700

    add button

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.get('/logout',
 
 
 // Listen to port
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function (req, res) {
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8000, function (req, res) {
     util.log("[GRADUAL] Listening", "green");
 });
 
